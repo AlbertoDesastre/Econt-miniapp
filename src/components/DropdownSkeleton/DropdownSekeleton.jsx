10c9b67b8d3a3db477849db9c414d17cb6cf7d86
Alberto_Desastre <armassantanaalberto@gmail.com>
2023-02-13 06:30:32 +0100

    Add SCSS

---
+++ src/components/DropdownSkeleton/DropdownSekeleton.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import "./DropdownSkeleton.scss";
+
+const DropdownSkeleton = () => {
+  // eslint-disable-next-line
+  const [displayList, setDisplayList] = useState(true);
+  const onClick = () => {
+    setDisplayList(!displayList);
+  };
+
+  return (
+    <div className="dropdown dropdown-skeleton">
+      <ul className="dropdown-ul">
+        <p>Econt offices</p>
+        {displayList && (
+          <li className="dropdown-li">
+            <p>Dedovo, in Plovdiv</p>
+            <p>Has express delivery? No </p>
+            <p>Postal code: 4106</p>
+          </li>
+        )}
+      </ul>
+      <p className="dropdown-arrow" onClick={onClick}>
+        {" "}
+        V{" "}
+      </p>
+    </div>
+  );
+};
+
+export default DropdownSkeleton;
