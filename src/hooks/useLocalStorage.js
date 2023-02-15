3292caedd82c2a0c5aee30574809d7da78c179ff
Alberto_Desastre <armassantanaalberto@gmail.com>
2023-02-15 16:59:54 +0100

    add main functions useLocalStorage

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,6 +1,32 @@
-const useLocalStorage = () => {
-  const saveItems = (newItem) => {};
-  const getItems = (itemName) => {};
+import { useState } from "react";
 
-  return { saveItems, getItems };
+const useLocalStorage = (itemName, initialValue) => {
+  const [items, setItems] = useState(initialValue);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  try {
+    const localStorageItem = localStorage.getItem(itemName);
+    let parsedItem;
+
+    if (!localStorageItem) {
+      localStorage.setItem(itemName, JSON.stringify(initialValue));
+      parsedItem = initialValue;
+    } else {
+      parsedItem = JSON.parse(localStorage.getItem(itemName));
+    }
+
+    setItems(parsedItem);
+    setLoading(false);
+  } catch (error) {
+    setError(error);
+  }
+
+  const saveItems = (newItem) => {
+    let newItemStringified = JSON.stringify(newItem);
+    localStorage.setItem(itemName, newItemStringified);
+    setItems(newItem);
+  };
+
+  return { saveItems, items, loading, error };
 };
