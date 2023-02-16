8537b521071c00715134c62d09768120eb38493c
Alberto_Desastre <armassantanaalberto@gmail.com>
2023-02-16 07:53:22 +0100

    Added state to select user. Not working the userSelectedInfo

--- src/components/UserProfile/UserProfile.jsx
+++ src/components/UserProfile/UserProfile.jsx
@@ -1,9 +1,28 @@
 import "./UserProfile.scss";
 
-const UserProfile = ({ id, name, region, city, isSelected }) => {
+const UserProfile = ({
+  id,
+  name,
+  region,
+  city,
+  userSelected,
+  setUserSelected,
+  setUserSelectedInfo,
+  users,
+}) => {
+  let active = false;
+  if (id === userSelected) {
+    active = true;
+  }
+
+  const onClick = () => {
+    setUserSelected(id);
+    /*     setUserSelectedInfo(users[userSelected]); */
+  };
+
   return (
-    <li key={id} className={"user-container"}>
-      <ul className="user-profile ">
+    <li key={id} className={`user-container ${active ? "active" : null}`}>
+      <ul className="user-profile " onClick={onClick}>
         <li className="user-name">
           <strong>
             <p>Username: </p>{" "}
